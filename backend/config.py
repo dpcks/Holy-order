29298ef5df0407225591de9d27bbbf9f1f34f54a
dpcks <jya05064@naver.com>
2026-07-20 21:34:20 +0900

    fix: 백엔드 시작 오류 수정 (VAPID 환경변수 기본값 추가)

--- backend/config.py
+++ backend/config.py
@@ -10,8 +10,8 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440
     CORS_ORIGINS: str = '["*"]'
-    VAPID_PUBLIC_KEY: str
-    VAPID_PRIVATE_KEY: str
+    VAPID_PUBLIC_KEY: str = "BAazjRtXAr7TtIMAwlLReivhL4tsfOma-ideBGfR87CrLroR4H01YaLiZ4pwmuqcF0Qy1V_0cF08MDATEEh-9t4"
+    VAPID_PRIVATE_KEY: str = "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgLUqbd46gxaG5Y3SM7aANSN3u_VXvDhlxBviJ7pGC3tehRANCAAQGs40bVwK-07SDAMJS0Xor4S-LbHzpmvonXgRn0fOwqy66EeB9NWGi4meKcJrqnBdEMtVf9HBdPDAwExBIfvbe"
     VAPID_CLAIM_EMAIL: str = "admin@example.com"
 
     model_config = SettingsConfigDict(env_file=os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
